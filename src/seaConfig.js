f15ec1f998b121d064204fd20b3d241f4757da83
534591395 <534591395.@qq.com>
2014-12-03 16:57:34 +0800

    marionette --1

---
+++ src/seaConfig.js
@@ -0,0 +1,9 @@
+seajs.config({
+    "alias": {
+        "underscore":"widget/underscore/src/underscore",
+        "backbone":"widget/backbone/src/backbone",
+        "marionette":"widget/marionette/src/marionette",
+        "backbone.localStorage":"widget/backbone.localStorage/src/backbone.localStorage"
+    },
+    base:'/src'
+});
